026b618d793b02a1027f50c2ca6bc6d9c73d43c3
joymush <joy.chemutai@student.moringaschool.com>
2022-08-27 22:30:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {'Content-Type':'application/json',
+        Accept:"application/json",
+    },
+    body:JSON.stringify({
+        name: name,
+        email: email,
+    }),
+
+    })
+    .then(res=>res.json())
+    .then(data=>{
+
+        document.body.innerHTML +=data.id;
+
+    })
+    .catch(errorMessage=>{
+
+    document.body.innerHTML +=errorMessage.message;
+    });
+}
+
